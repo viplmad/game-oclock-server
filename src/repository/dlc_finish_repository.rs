3001e92fa8d4f8a617a2527907729b29d5c862d1
viplmad <39457122+viplmad@users.noreply.github.com>
2023-01-17 23:47:00 +0100

    Fix finish fetch

--- src/repository/dlc_finish_repository.rs
+++ src/repository/dlc_finish_repository.rs
@@ -4,7 +4,7 @@ use sqlx::PgPool;
 use crate::errors::RepositoryError;
 use crate::query::dlc_finish_query;
 
-use super::base::{execute, exists_id, fetch_all_single, fetch_optional_single};
+use super::base::{execute, execute_return_single, exists_id, fetch_all_single};
 
 pub async fn find_first_by_dlc_id(
     pool: &PgPool,
@@ -12,7 +12,7 @@ pub async fn find_first_by_dlc_id(
     dlc_id: i32,
 ) -> Result<Option<NaiveDate>, RepositoryError> {
     let query = dlc_finish_query::select_max_by_user_id_and_dlc_id(user_id, dlc_id);
-    fetch_optional_single(pool, query).await
+    execute_return_single(pool, query).await
 }
 
 pub async fn find_all_by_dlc_id(
--- src/repository/game_finish_repository.rs
+++ src/repository/game_finish_repository.rs
@@ -4,7 +4,7 @@ use sqlx::PgPool;
 use crate::errors::RepositoryError;
 use crate::query::game_finish_query;
 
-use super::base::{execute, exists_id, fetch_all_single, fetch_optional_single};
+use super::base::{execute, execute_return_single, exists_id, fetch_all_single};
 
 pub async fn find_first_by_game_id(
     pool: &PgPool,
@@ -12,7 +12,7 @@ pub async fn find_first_by_game_id(
     game_id: i32,
 ) -> Result<Option<NaiveDate>, RepositoryError> {
     let query = game_finish_query::select_max_date_by_user_id_and_game_id(user_id, game_id);
-    fetch_optional_single(pool, query).await
+    execute_return_single(pool, query).await
 }
 
 pub async fn find_all_by_game_id(
