976875ead361079a253d8943af7ef6f3401e41cf
viplmad <39457122+viplmad@users.noreply.github.com>
2023-01-21 16:16:28 +0100

    Add important commands

--- client-gen.sh
+++ client-gen.sh
@@ -1,2 +1,7 @@
 # https://openapi-generator.tech/docs/installation/#jar
 java -jar openapi-generator-cli.jar generate -g dart -i public-api.json -c client-gen-conf.yml
+
+docker build -t n2tmad/game-col:0.1.0 .
+docker compose up -d
+docker compose down
+docker save n2tmad/game-col:0.1.0 -o game-col.tar
