4684253345a19aa0301c78bf5e7a0c3a3659ebfb
viplmad <39457122+viplmad@users.noreply.github.com>
2023-03-12 14:12:54 +0100

    Fix long standing api issues

--- src/models/id_param.rs
+++ src/models/id_param.rs
@@ -12,16 +12,16 @@ pub struct ItemIdAndRelatedId(pub i32, pub i32);
 
 #[derive(Deserialize, IntoParams)]
 pub struct StartEndDateQuery {
-    #[param(value_type = String)]
+    #[param(value_type = String, format = Date)]
     pub start_date: NaiveDate,
-    #[param(value_type = String)]
+    #[param(value_type = String, format = Date)]
     pub end_date: NaiveDate,
 }
 
 #[derive(Deserialize, IntoParams)]
 pub struct OptionalStartEndDateQuery {
-    #[param(value_type = Option<String>)]
+    #[param(value_type = Option<String>, format = Date)]
     pub start_date: Option<NaiveDate>,
-    #[param(value_type = Option<String>)]
+    #[param(value_type = Option<String>, format = Date)]
     pub end_date: Option<NaiveDate>,
 }
