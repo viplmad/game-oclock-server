db1f90020981c16b773dde3b22628f9767b7280a
viplmad <39457122+viplmad@users.noreply.github.com>
2023-08-24 15:41:43 +0200

    Fix migration

--- migrations/20230822115206_log_end_datetime.sql
+++ migrations/20230822115206_log_end_datetime.sql
@@ -2,7 +2,7 @@ ALTER TABLE ONLY "GameLog"
     ADD end_datetime timestamp without time zone;
 
 ALTER TABLE ONLY "GameLog"
-    ALTER COLUMN "time" SET NOT NULL;
+    ALTER COLUMN "time" SET NULL;
 
 UPDATE "GameLog"
     SET end_datetime = (datetime + "time");
